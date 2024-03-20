6ff8664ced2278f1b6d49e7b8fdb8c47ffd79f1e
jjungeeu <fbgjung@gmail.com>
2024-03-20 17:04:44 +0900

    3. Declare and Initialize variables

---
+++ src/variable/Var1.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+//        System.out.println(a);
+    }
+}
