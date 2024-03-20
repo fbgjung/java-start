f7fd89dc221bdc701d9f7ffe0e19a8f8ecc3fa21
jjungeeu <fbgjung@gmail.com>
2024-03-20 22:08:29 +0900

    4. Variable Type

---
+++ src/variable/Var8.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        //정수
+        byte b = 127; //-128 ~ 127
+        short s = 32767; //-32768 ~ 32767
+        int i = 2147483647; //-2147483648 ~ 2147483647 (약 21억)
+        long l = 9223372036854775807L; //-9223372036854775808 ~ 9223372036854775807
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
