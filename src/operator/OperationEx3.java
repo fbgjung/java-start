fd929677d31cade2eb8357b51319b5487d310ec8
jjungeeu <fbgjung@gmail.com>
2024-03-20 23:02:15 +0900

    7. Operator Question and Solution

---
+++ src/operator/OperationEx3.java
@@ -0,0 +1,9 @@
+package operator;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 80;
+        boolean result = score >= 80 && score <= 100;
+        System.out.println(result);
+    }
+}
