5cf7a1bba1e1ff0e74073623edd2bbeaea26c446
jjungeeu <fbgjung@gmail.com>
2024-03-21 14:26:09 +0900

    edit 12. Loop
    - seperate the file

---
+++ src/loop/NestedEx1.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class NestedEx1 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 9; i++){
+            for (int j = 1; j <= 9; j++){
+                System.out.println(i + "*" + j + "=" + i * j);
+            }
+        }
+    }
+}
