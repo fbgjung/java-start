cb2ac2d2ba41cfec8ba5cbed00ca71033ddc94a8
jjungeeu <fbgjung@gmail.com>
2024-03-22 11:10:28 +0900

    18. Two dimensional Array

---
+++ src/array/ArrayDi0.java
@@ -0,0 +1,25 @@
+package array;
+
+public class ArrayDi0 {
+    public static void main(String[] args) {
+        // 2x3 2차원 배열을 만든다.
+        int[][] arr = new int[2][3]; //행2, 열3
+        arr[0][0] = 1;
+        arr[0][1] = 2;
+        arr[0][2] = 3;
+        arr[1][0] = 4;
+        arr[1][1] = 5;
+        arr[1][2] = 6;
+
+        //0행 출력
+        System.out.print(arr[0][0] + " ");
+        System.out.print(arr[0][1] + " ");
+        System.out.print(arr[0][2] + " ");
+        System.out.println();
+
+        //1행 출력
+        System.out.print(arr[1][0] + " ");
+        System.out.print(arr[1][1] + " ");
+        System.out.print(arr[1][2] + " ");
+    }
+}
---
+++ src/array/ArrayDi3.java
@@ -0,0 +1,18 @@
+package array;
+
+public class ArrayDi3 {
+    public static void main(String[] args) {
+        // 2x3 2차원 배열을 만든다.
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6}
+        }; //행2, 열3
+
+        for(int row = 0; row < arr.length; row++){
+            for (int column = 0; column < arr[row].length; column++){
+                System.out.print(arr[row][column] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
